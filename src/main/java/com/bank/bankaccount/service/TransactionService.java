f1befb7dd5ad7f8f5d2f3688be7ea7ec52b444e9
Fariha Khan <fariha.khan10@outlook.com>
2022-03-19 21:11:06 +0500

    added get transactions and credit transaction in transaction service

--- src/main/java/com/bank/bankaccount/service/TransactionService.java
+++ src/main/java/com/bank/bankaccount/service/TransactionService.java
@@ -1,7 +1,10 @@
 package com.bank.bankaccount.service;
 
+import com.bank.bankaccount.dto.CustomerDTO;
 import com.bank.bankaccount.model.Account;
+
 
 public interface TransactionService {
     Long performGLToCustomerTransfer(Account fromAccount, Account toAccount, Double amount);
+    void getCustomerTransactions(CustomerDTO customer);
 }
--- src/main/java/com/bank/bankaccount/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/bank/bankaccount/service/impl/TransactionServiceImpl.java
@@ -1,11 +1,15 @@
 package com.bank.bankaccount.service.impl;
 
+import com.bank.bankaccount.dto.AccountDTO;
+import com.bank.bankaccount.dto.CustomerDTO;
+import com.bank.bankaccount.dto.TransactionDTO;
 import com.bank.bankaccount.enums.Error;
 import com.bank.bankaccount.enums.TransactionStatus;
 import com.bank.bankaccount.enums.TransactionType;
 import com.bank.bankaccount.exception.BankAccountCustomException;
 import com.bank.bankaccount.model.Account;
 import com.bank.bankaccount.model.Transaction;
+import com.bank.bankaccount.repository.AccountsRepository;
 import com.bank.bankaccount.repository.TransactionRepository;
 import com.bank.bankaccount.service.AccountsService;
 import com.bank.bankaccount.service.TransactionService;
@@ -13,44 +17,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Transactional
 public class TransactionServiceImpl implements TransactionService {
 
     @Autowired TransactionRepository transactionRepository;
     @Autowired AccountsService accountsService;
+    @Autowired AccountsRepository accountsRepository;
 
     @Override
     @Transactional
     public Long performGLToCustomerTransfer(Account fromAccount, Account toAccount, Double amount) {
-//        Long txnId = null;
 
         // validate txn
         validateTransaction(fromAccount, toAccount, amount);
         // prepare debit txn
         Transaction txn = prepareTransaction(fromAccount, toAccount, amount, TransactionType.GL2C);
         // debit account balance deduct in case it is customer account
+        // credit balance to payee
+        creditBalance(toAccount, amount);
         // save txn
         txn = transactionRepository.save(txn);
+
         return txn.getId();
     }
 
-    private boolean validateTransaction(Account fromAccount, Account toAccount, Double amount) {
+    @Override
+    public void getCustomerTransactions(CustomerDTO customer) {
+
+        for (AccountDTO acc :
+                customer.getAccounts()) {
+            List<TransactionDTO> txns = new ArrayList<>(getAccountTransactions(acc.getId()));
+            acc.setTransactions(txns);
+        }
+
+    }
+
+    private List<TransactionDTO> getAccountTransactions(Long accountId) {
+
+        // get all transactions
+        List<Transaction> txns = transactionRepository.getAccountTransactions(accountId);
+        return txns.stream().map(Transaction::toDTO).collect(Collectors.toList());
+    }
+
+    private void creditBalance(Account account, Double amount) {
+        Double currBalance = account.getBalance();
+        account.setBalance(currBalance + amount);
+        accountsRepository.save(account);
+    }
+
+    private void validateTransaction(Account fromAccount, Account toAccount, Double amount) {
 
         // validate debit account
         validateAccount(fromAccount, "debit account");
         // validate credit account
         validateAccount(toAccount, "credit account");
         // validate debit account balance
-        Double debitBalance = accountsService.checkBalance(fromAccount);
+
+        Double debitBalance = accountsRepository.getAccountBalance(fromAccount.getAccountNo());
         if(debitBalance < amount)
             throw new BankAccountCustomException(Error.LOW_BALANCE.getCode(), Error.LOW_BALANCE.getMsg());
 
-        return true;
     }
 
     private void validateAccount(Account account, String errorMag) {
-        if(!accountsService.accountExists(account))
+        if(!accountsService.accountExists(account.toDTO()))
             throw new BankAccountCustomException(Error.INVALID.getCode(), String.format(Error.INVALID.getMsg(), errorMag));
 
     }
